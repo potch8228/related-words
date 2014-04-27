51172d86dfc5e92352b144155c1a2d0119f170c0
Tatsuya Kobayashi <pikopiko28@gmail.com>
2014-04-27 18:36:27 +0900

    Finish up for the first version
    Add argument parsing, CSV I/O, character encoding

--- related_word.py
+++ related_word.py
@@ -1,13 +1,20 @@
 # -*- coding: utf8 -*-
 
+import argparse
 import csv
 import json
 import urllib2
-from xml.dom import minidom
 from contextlib import closing
+from xml.dom import minidom
 
-BASE_URL_JSON  = 'http://suggestqueries.google.com/complete/search?client=firefox&q='
-BASE_URL       = 'http://suggestqueries.google.com/complete/search?client=toolbar&q='
+BASE_URL_JSON = 'http://suggestqueries.google.com/complete/search?client=firefox&q='
+BASE_URL      = 'http://suggestqueries.google.com/complete/search?client=toolbar&q='
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input_csv',  help='input word list in CSV file format')
+parser.add_argument('output_csv', help='output CSV file')
+parser.add_argument('-c', '--combination', \
+        type=int, default=3, help='maximum word combinations to search')
 
 def build_search_url(request_query, json=False):
     """Build up search url that is passed to defined url"""
@@ -38,10 +45,11 @@ def parse_search_result(search_result_list, json=False):
     if json:
         return json.loads(search_result_list)[1]
 
-    search_result = minidom.parseString(search_result_list).getElementsByTagName('suggestion')
+    search_result = minidom.parseString(search_result_list) \
+                            .getElementsByTagName('suggestion')
     return set([result.attributes['data'].value for result in search_result])
 
-def search_relative(input_list, result_dict = {}):
+def search_relative(input_list, result_dict = {}, max_combo=3):
     """Actual search function to organize all of functions above(main function?)
 
     dispatch the search urls as much as input_list has
@@ -51,13 +59,17 @@ def search_relative(input_list, result_dict = {}):
         raise ValueException('No input_list')
 
     if len(input_list) is 0 \
-            or _check_dict_depth(result_dict):
+            or _check_dict_depth(result_dict, max_combo):
         return result_dict
 
     for word in input_list:
-        if _check_combo_length(word):
+        if _check_combo_length(word, max_combo):
             continue
-        fetch_result = parse_search_result(fetch_search_url(build_search_url(word)))
+        fetch_result = parse_search_result( \
+                fetch_search_url( \
+                    build_search_url(word) \
+                ) \
+            )
         if word in fetch_result:
             fetch_result.remove(word)
         if word not in result_dict:
@@ -67,21 +79,21 @@ def search_relative(input_list, result_dict = {}):
         search_relative(fetch_result, result_dict)
     return result_dict
 
-def _check_combo_length(query_word, max_combo=3):
+def _check_combo_length(query_word, max_combo):
     if query_word is None:
         raise ValueException('No query')
-    return len(query_word.split()) > max_combo
+    return len(query_word.split()) >= max_combo
 
-def _check_word_combo_length(query_set, max_combo=3):
+def _check_word_combo_length(query_set, max_combo):
     if query_set is None:
         raise ValueException('No query')
 
     for item in query_set:
-        if len(item.split()) > max_combo:
+        if len(item.split()) >= max_combo:
             return True
     return False
 
-def _check_dict_depth(word_dict, max_deep=3):
+def _check_dict_depth(word_dict, max_deep):
     """Checks if any of dict entries reaches max_deep
     Default deepness is 3
     Since the result word is either a single word or a combination of
@@ -92,49 +104,105 @@ def _check_dict_depth(word_dict, max_deep=3):
         raise ValueException('No word_dict')
 
     result = False
-    for values in word_dict.values():
-        result = _check_word_combo_length(values)
+    for value in word_dict.values():
+        result = _check_word_combo_length(value, max_deep)
     return result
 
-def read_csv(file_path):
+def read_csv(file_path, encoding='shift_jis'):
     if file_path is None:
         raise ValueException('No file_path')
 
     input_list = []
-    with open(file_path, 'r') as csv_file:
-        for input_line in csv.reader(csv_file):
-           input_list.extend(input_line)
+    with open(file_path, 'rb') as csv_file:
+        reader = UnicodeReader(csv_file, encoding=encoding)
+        for input_data in reader:
+            input_list.extend(input_data)
 
     return input_list
 
-def output_format(search_data):
-    """build up string object with comma splited, multi-linebroken string"""
-    if search_data is None:
-        raise ValueException('No search_data')
-
-    _output = ''
-    return False
-
-def write_csv(file_path, search_data):
-    """Open the file descripter
-    write out all search data, the search data will be in either list or dict
-    Close the file descripter
-    """
+def write_csv(file_path, search_data, encoding='shift_jis'):
     if file_path is None:
         raise ValueException('No file_path to write')
 
-    with open(file_path, 'w') as csv_file:
-        csv_file.write(output_format(search_data))
+    with open(file_path, 'wb') as csv_file:
+        writer = UnicodeWriter(csv_file, encoding=encoding)
+        for key in sorted(search_data):
+            _output_list = list(search_data[key])
+            _output_list.insert(0, key)
+            writer.writerow(_output_list)
+
+import codecs, cStringIO
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+#        self.writer.writerow([s for s in row])
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
 
 def main():
-    """TODO List:
-    Get the initial inputs from given CSV file in list structure
-    Throw these inputs into google suggestion
-    - Get these results in list form
-    - recursively find words, use dict and set
-    Gather the results in CSV format, and output(on stdout?)
-    """
-    pass
+    args = parser.parse_args()
+    if args.input_csv is None \
+        or args.output_csv is None:
+        parser.print_usage()
+        exit(1)
+
+    write_csv(args.output_csv, \
+            search_relative( \
+                read_csv(args.input_csv), max_combo=args.combination \
+            ) \
+    )
 
 if __name__ == '__main__':
     main()
--- test_related_word.py
+++ test_related_word.py
@@ -1,19 +1,78 @@
 # -*- encoding: utf8 -*-
 import unittest
 import related_word
+from os import path
 
 class TestRelatedWord(unittest.TestCase):
-#    def test_search_result(self):
-#        raw_result_list = related_word.fetch_search_url(related_word.build_search_url(u'ロキソニン'))
-#        result_list = related_word.parse_search_result(raw_result_list)
-#        self.assertEqual(len(result_list), 10, 'Probably suggestions are less than 10')
+    def setUp(self):
+        self.sample_list_en = [u'test', u'sample']
+        self.sample_list_ja = [u'ロキソニン', u'ボルタレン', u'バファリン']
+        self.sample_dict    = { \
+                                u'test' :set([u'a',u'b',u'c',u'd']), \
+                                u'test2':set([u'e',u'f',u'g',u'h']) \
+                                }
+        self.sample_dict_ja = { \
+                                u'テスト' :set([u'あ',u'い',u'う',u'え']), \
+                                u'テスト2':set([u'か',u'き',u'く',u'け']) \
+                                }
+        self.single_search_word_en = u'test'
+        self.single_search_word_ja = u'ロキソニン'
 
-    def test_search_relative(self):
-        sample_list = [u'ロキソニン', u'ボルタレン']
-#        sample_list = [u'test', u'sample']
-        result_dict = related_word.search_relative(sample_list)
+        self.test_input_csv  = 'test_result/test_input.csv'
+        self.test_output_csv = 'test_result/test_output.csv'
+
+        self.test_input_csv_ja  = 'test_result/test_input_ja.csv'
+        self.test_output_csv_ja = 'test_result/test_output_ja.csv'
+
+    def test_search_result(self):
+        raw_result_list = related_word.fetch_search_url( \
+                related_word.build_search_url(self.single_search_word_en) \
+                )
+        result_list     = related_word.parse_search_result(raw_result_list)
+        self.assertEqual(len(result_list), 10,  \
+                'Probably suggestions are less than 10')
+
+    def test_search_relative_english(self):
+        result_dict = related_word.search_relative(self.sample_list_en)
         self.assertNotEqual(len(result_dict), 0, 'Probably good structure')
-        print result_dict
+#        print result_dict
+
+    def test_search_relative_japanese(self):
+        result_dict = related_word.search_relative(self.sample_list_ja)
+        self.assertNotEqual(len(result_dict), 0, 'Probably good structure')
+#        print result_dict
+
+    def test_read_csv(self):
+        result_list = related_word.read_csv(self.test_input_csv, encoding='utf-8')
+        self.assertNotEqual(len(result_list), 0, 'Probably not reading...')
+        self.assertTrue(self.single_search_word_en in result_list, \
+                'Probably miss-conversion')
+
+    def test_read_csv_japanese(self):
+        result_list = related_word.read_csv(self.test_input_csv_ja, encoding='utf-8')
+        self.assertNotEqual(len(result_list), 0, 'Probably not reading...')
+        self.assertTrue(self.single_search_word_ja in result_list, \
+                'Probably miss-conversion')
+
+    def test_write_csv(self):
+        related_word.write_csv(self.test_output_csv,self.sample_dict)
+        self.assertTrue(path.exists(self.test_output_csv), 'No output file exist!')
+        csv_content = related_word.read_csv(self.test_output_csv)
+        len_dict = len(self.sample_dict)
+        for v in self.sample_dict.values():
+            len_dict += len(v)
+        self.assertEqual(len(csv_content), len_dict, \
+                'Input and Output has something wrong...' + str(len(csv_content)))
+
+    def test_write_csv_japanese(self):
+        related_word.write_csv(self.test_output_csv_ja,self.sample_dict_ja)
+        self.assertTrue(path.exists(self.test_output_csv_ja), 'No output file exist!')
+        csv_content = related_word.read_csv(self.test_output_csv_ja)
+        len_dict = len(self.sample_dict_ja)
+        for v in self.sample_dict.values():
+            len_dict += len(v)
+        self.assertEqual(len(csv_content), len_dict, \
+                'Input and Output has something wrong...' + str(len(csv_content)))
 
 if __name__ == '__main__':
     unittest.main()
